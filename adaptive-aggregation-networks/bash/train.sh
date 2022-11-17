1db6c9d0a46105a5d75212994f7594cc8675c1fd
xiexuchen <545238379@qq.com>
2022-11-17 23:04:57 +0800

    train

--- adaptive-aggregation-networks/bash/train.sh
+++ adaptive-aggregation-networks/bash/train.sh
@@ -4,8 +4,7 @@ for seed in 32 40 50; do
     for init_cls in 10 20 50; do
         ${PYTHON} ../main.py --nb_cl_fg=${init_cls} --nb_cl=${init_cls} \
         --gpu=0 --random_seed=${seed} --baseline="lucir" \
-        --branch_mode="dual" --branch_1="ss" --branch_2="free" \ 
+        --branch_mode="dual" --branch_1="ss" --branch_2="free" \
         --dataset="cifar100"
     done
 done
-    

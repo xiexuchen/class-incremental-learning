f51e964ec1b6f71a955cc9d0df7b8b8fb6268921
xiexuchen <545238379@qq.com>
2022-11-17 23:01:05 +0800

    train

--- adaptive-aggregation-networks/bash/train.sh
+++ adaptive-aggregation-networks/bash/train.sh
@@ -2,7 +2,6 @@
 PYTHON='/public/home/daijiahao/anaconda3/bin/python'
 for seed in 32 40 50; do
     for init_cls in 10 20 50; do
-        increment=${init_cls}
         ${PYTHON} ../main.py --nb_cl_fg=${init_cls} --nb_cl=${init_cls} \
         --gpu=0 --random_seed=${seed} --baseline="lucir" \
         --branch_mode="dual" --branch_1="ss" --branch_2="free" \ 

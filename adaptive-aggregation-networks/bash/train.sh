e0f5040ea47f3e47156577340beb70aae670d0e4
xiexuchen <545238379@qq.com>
2022-11-18 10:14:00 +0800

    train aanet without stop gpu

--- adaptive-aggregation-networks/bash/train.sh
+++ adaptive-aggregation-networks/bash/train.sh
@@ -5,6 +5,6 @@ for seed in 32 40 50; do
         ${PYTHON} ../main.py --nb_cl_fg=${init_cls} --nb_cl=${init_cls} \
         --gpu=0 --random_seed=${seed} --baseline="lucir" \
         --branch_mode="dual" --branch_1="ss" --branch_2="free" \
-        --dataset="cifar100"
+        --dataset="cifar100" --disable_gpu_occupancy=1
     done
 done

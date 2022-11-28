c9cb2c434eda106cbe720a886ad67a955dcdff61
xiexuchen <545238379@qq.com>
2022-11-28 13:48:13 +0800

    cifar100,scratch

--- adaptive-aggregation-networks/bash/train_cifar100.sh
+++ adaptive-aggregation-networks/bash/train_cifar100.sh
@@ -4,7 +4,7 @@ PYTHON='/public/home/daijiahao/anaconda3/bin/python'
 for seed in 32 40 50; do
     for init_cls in 5 10; do
         ${PYTHON} ../main.py --nb_cl_fg=${init_cls} --nb_cl=${init_cls} \
-        --gpu=3 --random_seed=${seed} --baseline="lucir" \
+        --gpu=0 --random_seed=${seed} --baseline="lucir" \
         --branch_mode="dual" --branch_1="ss" --branch_2="free" \
         --dataset="cifar100" --epochs=160 --image_size=224 --disable_gpu_occupancy
     done
